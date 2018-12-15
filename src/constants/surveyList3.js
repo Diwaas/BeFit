5feb4fb9b71d50671264ffe18933cfa5eed745c4
rdevadass <rdevadass@paypal.com>
2018-12-15 20:09:47 +0530

    Added test json data

---
+++ src/constants/surveyList3.js
@@ -0,0 +1,13 @@
+let surveyList3 = {
+   "surveyId":"SVY001",
+   "nodeId":1,
+   "parentNodeId":0,
+   "question":"Question with Input box ?",
+   "inputType" : "textbox",
+   "option":'',
+   "result":'Result for Question with Input box ?',
+   "subSurvey":[
+   ]
+};
+
+export default surveyList;
